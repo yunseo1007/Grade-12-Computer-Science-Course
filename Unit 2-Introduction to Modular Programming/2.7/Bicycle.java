71fb9db0c613313a1979ef650f8d205f1f072f7f
Yunseo Ha <95513884+yunseo1007@users.noreply.github.com>
2022-02-16 13:52:18 -0500

    Add files via upload

---
+++ Unit 2-Introduction to Modular Programming/2.7/Bicycle.java
@@ -0,0 +1,25 @@
+
+public class Bicycle implements Vehicle{
+
+	int speed;
+	int gear;
+	@Override
+	public void changeGear(int newGear) {
+		// TODO Auto-generated method stub
+		gear = newGear;
+	}
+	@Override
+	public void speedUp(int increment) {
+		// TODO Auto-generated method stub
+		speed = speed + increment;
+	}
+	@Override
+	public void applyBrakes(int decrement) {
+		// TODO Auto-generated method stub
+		speed = speed - decrement;
+	}
+	public void printStates() {
+		System.out.println("speed: " + speed + " gear: " + gear);
+	}
+	
+}
