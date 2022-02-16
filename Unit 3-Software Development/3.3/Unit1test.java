45344114d88579350e31696398724898513216af
Yunseo Ha <95513884+yunseo1007@users.noreply.github.com>
2022-02-16 13:45:30 -0500

    Add files via upload

---
+++ Unit 3-Software Development/3.3/Unit1test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Unit1test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 0;
+		int b = 6;
+		System.out.println("The number " + b + " divided by " + a + " will result in " + b/a);
+	}
+		
+	}
+
+
